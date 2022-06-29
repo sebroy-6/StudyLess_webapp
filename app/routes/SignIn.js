b3c4898cee59200f0caeb08129ce714005ce2162
sebroy-6 <sebastien-6.roy@polymtl.ca>
2022-06-29 08:22:49 -0400

    added json database for users and working on user identification

--- app/routes/SignIn.js
+++ app/routes/SignIn.js
@@ -1,17 +1,10 @@
+const jsonScript = require("../scripts/json_scripts");
 const express = require("express");
 const router = express.Router();
-const pagesRoute = "../public/pages"
+const pagesRoute = "../public/pages";
 const bodyParser = require('body-parser'); // Middleware 
 
 router.use(bodyParser.urlencoded({ extended: false }));
-
-
-function verifyLogin(uname, psw) {
-  if (uname.length > 0 && psw.length > 0)
-    return true;
-  else 
-    return false
-}
 
 router.get("/", (req, res) => {
     res.render(pagesRoute + "/signIn");
@@ -21,7 +14,8 @@ router.post("/", (req, res) => {
   // Insert Login Code Here
   let username = req.body.uname;
   let password = req.body.psw;
-  res.send(verifyLogin(username, password));
+  jsonScript.addUser(username, password);
+  res.render(pagesRoute + "/signIn");
 });
 
 module.exports = router;
---
+++ app/scripts/json_scripts.js
@@ -0,0 +1,29 @@
+// get the data from a json file and store it in a variable
+const fs = require('fs');
+
+const userJsonPath = "./app/data/users.json";
+
+function getData(fileName = userJsonPath) {
+    let data = fs.readFileSync(fileName, 'utf8');
+    return JSON.parse(data);
+}
+
+function getUser(username) {
+    return getData(userJsonPath).find(user => user.username === username);
+}
+
+function addUser(username, password) {
+    let users = getData(userJsonPath);
+    let user = { username: username, password: password };
+    if (getUser(username) === undefined){
+        users.push(user);
+        fs.writeFileSync(userJsonPath, JSON.stringify(users));
+        console.log(user.username + " has been added to the database");
+        return user;
+    }
+    console.log(`The user ${username} already exists`);
+    return user;
+}
+
+module.exports = { getUser, addUser };
+
