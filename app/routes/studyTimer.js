a14192a8cf9a4ad2d2695f2398f09cd51b3e6397
sebroy-6 <sebastien-6.roy@polymtl.ca>
2022-07-17 11:58:57 -0400

    made timer states functional (not final)

--- app/routes/studyTimer.js
+++ app/routes/studyTimer.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const STUDY_TIME = "00:10";
+const STUDY_TIME = "00:25";
 const BREAK_TIME = "00:05";
-const TIME_OUT_TIME = "00:10";
+const TIME_OUT_TIME = "00:15";
 
 router.get("/", (req, res) => {
     if(req.session.userid)
@@ -16,41 +16,21 @@ router.post("/", (req, res) => {
     if(req.session.userid === undefined)
         res.redirect("/");
     else {
-        let timerMode;
-        if (req.body.timerState) {
-            timerMode = req.body.timerState;
-            let responseJson = {};
-            switch (timerMode) {
-                case "study":
-                    responseJson.newTime = STUDY_TIME;
-                    break;
-                case "break":
-                    responseJson.newTime = BREAK_TIME;
-                    break;
-                case "time-out":
-                    responseJson.newTime = TIME_OUT_TIME;
-                    break;
-                default:
-                    responseJson.newTime = "00:00:00";
+        let responseJson = {};
+        switch (req.body.timerState) {
+            case "study":
+                responseJson.newTime = STUDY_TIME;
+                break;
+            case "break":
+                responseJson.newTime = BREAK_TIME;
+                break;
+            case "time-out":
+                responseJson.newTime = TIME_OUT_TIME;
+                break;
+            default:
+                responseJson.newTime = "00:00:00";
             }
-            res.json(responseJson);
-        }
-        else if (req.body.button) {
-            timerMode = req.body.button;
-            switch (timerMode) {
-                case "study":
-                    res.render("studyTimer", { time: STUDY_TIME });
-                    break;
-                case "break":
-                    res.render("studyTimer", { time: BREAK_TIME });
-                    break;
-                case "time-out":
-                    res.render("studyTimer", { time: TIME_OUT_TIME });
-                    break;
-                default:
-                    res.render("studyTimer", { time: "00:00:00"});
-            } 
-        }
+        res.json(responseJson);
     }
 });
 
--- public/scripts/timer.js
+++ public/scripts/timer.js
@@ -2,8 +2,12 @@ const PORT = 5000;
 const SECONDS_IN_MINUTE = 60;
 const SECONDS_IN_HOUR = 3600;
 
-const ACTIVE_COLOR = "lightgrey";
+const ACTIVE_COLOR = "grey";
 const DEFAULT_COLOR = "white";
+
+const STUDY_MODE = "study";
+const BREAK_MODE = "break";
+const TIMEOUT_MODE = "time-out";
 
 function isOrderedTime(time) {
     if (typeof(time) === "string") {
@@ -66,11 +70,11 @@ function getCurrentTimerState() {
     let timeoutTagColor = document.getElementById("time-outTag").style.color;
     
     if (breakTagColor === ACTIVE_COLOR)
-        return "break";
+        return BREAK_MODE;
     else if (timeoutTagColor === ACTIVE_COLOR)
-        return "time-out";
+        return TIMEOUT_MODE;
     else 
-        return "study";
+        return STUDY_MODE;
 }
 
 
@@ -94,25 +98,56 @@ function orderedTimeToSeconds(time) {
 
 function shuffleTimerState() {
     let nextState;
-    switch(getCurrentTimerState()) {
-        case "study":
-            nextState = "break";
+    switch (getCurrentTimerState()) {
+        case STUDY_MODE:
+            nextState = BREAK_MODE;
             document.getElementById("breakTag").style.color = ACTIVE_COLOR;
             document.getElementById("studyTag").style.color = DEFAULT_COLOR;
             break;
-        case "break":
-            nextState = "time-out";
+        case BREAK_MODE:
+            nextState = TIMEOUT_MODE;
             document.getElementById("time-outTag").style.color = ACTIVE_COLOR;
             document.getElementById("breakTag").style.color = DEFAULT_COLOR;
             break;
-        case "time-out":
-            nextState = "study";
+        case TIMEOUT_MODE:
+            nextState = STUDY_MODE;
             document.getElementById("studyTag").style.color = ACTIVE_COLOR;
             document.getElementById("time-outTag").style.color = DEFAULT_COLOR;
             break;
     }
-    document.getElementById("start").innerHTML = "START";
     return nextState;
+}
+
+
+function setTimerByState(timerState) {
+    document.getElementById("start").innerHTML = "START";
+    if (timerState === STUDY_MODE) {
+        document.getElementById("studyTag").style.color = ACTIVE_COLOR;
+        document.getElementById("breakTag").style.color = DEFAULT_COLOR;
+        document.getElementById("time-outTag").style.color = DEFAULT_COLOR;
+    }
+    else if (timerState === BREAK_MODE) {
+        document.getElementById("studyTag").style.color = DEFAULT_COLOR;
+        document.getElementById("breakTag").style.color = ACTIVE_COLOR;
+        document.getElementById("time-outTag").style.color = DEFAULT_COLOR;
+    }
+    else if (timerState === TIMEOUT_MODE) {
+        document.getElementById("studyTag").style.color = DEFAULT_COLOR;
+        document.getElementById("breakTag").style.color = DEFAULT_COLOR;
+        document.getElementById("time-outTag").style.color = ACTIVE_COLOR;
+    }
+
+    const data = { timerState };
+    fetch(`/studyTimer`, { 
+        method : 'POST', 
+        headers : {
+            'Content-Type': 'application/json'
+        },
+        body : JSON.stringify(data)
+    }).then((resp) => resp.json()).then((resp) => {
+        document.getElementById("browserTab").innerHTML = `${resp.newTime} - POLYSTUDY`;
+        document.getElementById("clock").innerHTML = resp.newTime;
+    });
 }
 
 
@@ -126,25 +161,20 @@ function toggleTimer(seconds, outputId, buttonId) {
                 let timeLeft = seconds - Math.floor((new Date() - start) / 1000);
                 let buttonState = document.getElementById(buttonId).innerHTML;
 
-                if (timeLeft && buttonState.toLowerCase() === "stop")
-                    document.getElementById(outputId).innerHTML = secondsToOrderedTime(timeLeft);
+                if (timeLeft && buttonState.toLowerCase() === "stop") {
+                    let orderedTimeLeft = secondsToOrderedTime(timeLeft);
+                    document.getElementById(outputId).innerHTML = orderedTimeLeft;
+                    document.getElementById("browserTab").innerHTML = `${orderedTimeLeft} - POLYSTUDY`;
+                }
                 else if (buttonState.toLowerCase() === "start"){
                     clearInterval(timer);
                 }
                 else {
                     clearInterval(timer);
+                    console.log(new Date() - start);
+                    document.getElementById("browserTab").innerHTML = `0 - POLYSTUDY`;
                     document.getElementById(outputId).innerHTML = "0"; 
-                    let timerState = shuffleTimerState();
-                    const data = { timerState };
-                    fetch(`/studyTimer`, { 
-                        method : 'POST', 
-                        headers : {
-                            'Content-Type': 'application/json'
-                        },
-                        body : JSON.stringify(data)
-                    }).then((resp) => resp.json()).then((resp) => {
-                        document.getElementById(outputId).innerHTML = resp.newTime;
-                    });
+                    setTimerByState(shuffleTimerState());
                 }
             }, 50);
         }
